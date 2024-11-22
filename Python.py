3e6c6750a4d1dc26cddf1b66d752e4d4bade44fa
Killeryoung <crawltasher@gmail.com>
2024-11-22 20:26:39 +0200

    Синхронизация: удаление лишних файлов

--- Python.py
+++
@@ -1,4 +0,0 @@
-first_string = input("Введіть перший рядок: ")
-second_string = input("Введіть другий рядок: ")
-result = ''.join([char for char in first_string if char not in second_string])
-print("Символи, які є у першому рядку і яких немає у другому:", result)
--- Python1.py
+++
@@ -1,3 +0,0 @@
-numbers = input("Введіть числа, розділені пробілами: ")
-result = ' '.join([f"x={num}" for num in numbers.split()])
-print("Результат:", result)
